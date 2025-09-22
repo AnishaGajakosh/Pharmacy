6a7a346f1e2c17b8ffef3ac3a008e82cd28b7bd0
AnishaGajakosh <anishagajakosh@gmail.com>
2025-09-22 11:53:30 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
-
-const productSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String,
-  sku: String,
-  image: String,
-  category: String
-}, { timestamps: true });
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: String,
+    sku: String,
+    image: String,
+    category: String,
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Product", productSchema);
